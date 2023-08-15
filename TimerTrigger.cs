1d17cb2d347e8000bbefd380070435561c419e03
Andrejs Agejevs <esneko@gmail.com>
2023-08-15 22:34:09 +0300

    Timer

---
+++ TimerTrigger.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace az_func_proj
+{
+    public class TimerTrigger
+    {
+        [FunctionName("TimerTrigger")]
+        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
