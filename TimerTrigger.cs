8ecb13752edf9526f4ad9f08ceb9364e7afa699f
Andrejs Agejevs <esneko@gmail.com>
2023-08-15 23:02:15 +0300

    Update time span to one minute
    2023-08-15T20:00:00Z   [Information]   C# Timer trigger function executed at: 08/15/2023 20:00:00

--- TimerTrigger.cs
+++ TimerTrigger.cs
@@ -8,7 +8,7 @@ namespace az_func_proj
     public class TimerTrigger
     {
         [FunctionName("TimerTrigger")]
-        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
