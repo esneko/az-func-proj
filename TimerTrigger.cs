4994f9954c025c636368fb361fef1f6f7f9d7888
Andrejs Agejevs <esneko@gmail.com>
2023-08-16 16:35:57 +0300

    Inject HttpClient into Azure Function

--- TimerTrigger.cs
+++ TimerTrigger.cs
@@ -1,16 +1,46 @@
 using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Extensions;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace az_func_proj
+namespace az_func_proj;
+
+public class TimerTrigger
 {
-    public class TimerTrigger
+    private readonly HttpClient _client;
+
+    public TimerTrigger(HttpClient httpClient) //, IStorageService storage
     {
-        [FunctionName("TimerTrigger")]
-        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        this._client = httpClient;
+        // this._storage = storage;
+    }
+
+    [FunctionName("TimerTrigger")]
+    public void Run(
+        [TimerTrigger("0 */1 * * * *")] TimerInfo myTimer,
+        ILogger log)
+    {
+        log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        try
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var response = await _client.GetAsync("https://api.publicapis.org/"); // random?auth=null
+            var message = _service.GetMessage();
+            log.LogInformation($"C# Timer trigger function called the API: {message}");
+        }
+        catch (System.Exception)
+        {
+            log.LogInformation($"C# Timer trigger function failed to call the API");
+            throw;
         }
     }
 }
