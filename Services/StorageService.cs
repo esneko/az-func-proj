cdb2d9d714749c0553df1ed9f5f37b97dbb24a92
Andrejs Agejevs <esneko@gmail.com>
2023-08-18 00:26:53 +0300

    Extract _containerName

--- Services/StorageService.cs
+++ Services/StorageService.cs
@@ -12,25 +12,27 @@ namespace AzFuncProj.Storage.Service;
 public class StorageService : IStorageService
 {
   private string _connectionString;
+  private string _containerName;
   private BlobServiceClient _client;
 
   public StorageService()
   {
     _connectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=ateatestrgbbc5;AccountKey=lmgXjRiHpymHg5lbdRtIy51ohHx9UC73LuB5xvzEQxIpyxL56B7E+8fhHyZig2T7uCVsaqRw0JrI+AStV2qJOQ==";
+    _containerName = "azfuncst"
     _client = new BlobServiceClient(_connectionString);
   }
 
   public async Task<bool> SaveFile(string blobName, string blobData)
   {
-    BlobContainerClient containerClient = _client.GetBlobContainerClient("azfuncst");
+    BlobContainerClient containerClient = _client.GetBlobContainerClient(_containerName");
     BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+    await blobClient.UploadAsync(BinaryData.FromString(blobData), overwrite: true);
 
     // using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(blobData)))
     // {
     //   await blobClient.UploadAsync(ms);
     // }
-
-    await blobClient.UploadAsync(BinaryData.FromString(blobData), overwrite: true);
 
     return true;
   }
